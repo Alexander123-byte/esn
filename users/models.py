6f1b164979f23da2006a90fc2c21b9ea9a148291
Alexander <san.juckov2017@yandex.ru>
2024-08-19 16:50:20 +0300

    PEP8

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .managers import CustomUserManager
 
+
 class User(AbstractUser):
     username = None
     first_name = models.CharField(max_length=50, verbose_name='Имя')
